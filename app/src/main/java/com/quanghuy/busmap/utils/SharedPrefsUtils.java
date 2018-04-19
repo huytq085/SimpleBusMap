1beea6e1a23b0511a615322b630c6852027de796
tranquanghuy341 <tranquanghuy341@gmail.com>
2018-04-19 15:38:00 +0700

    update SharedPrefsUtils

--- app/src/main/java/com/quanghuy/busmap/utils/SharedPrefsUtils.java
+++ app/src/main/java/com/quanghuy/busmap/utils/SharedPrefsUtils.java
@@ -12,20 +12,21 @@ import com.quanghuy.busmap.entity.User;
 
 public class SharedPrefsUtils {
     private static final String CURRENT_USER = "currentUser";
+
+    public static SharedPreferences getPrefs(Context context) {
+        return context.getSharedPreferences(Constants.PACKAGE_NAME, Context.MODE_PRIVATE);
+    }
     public static User getCurrentUser(Context context) {
-        SharedPreferences mPrefs = context.getSharedPreferences(Constants.PACKAGE_NAME, Context.MODE_PRIVATE);
-        String currentUserString = mPrefs.getString(CURRENT_USER, "");
+        String currentUserString = getPrefs(context).getString(CURRENT_USER, "");
         User currentUser = JsonUtils.decode(currentUserString, User.class);
         return currentUser;
     }
 
     public static void setCurrentUser(Context context, User user) {
-        SharedPreferences mPrefs = context.getSharedPreferences(Constants.PACKAGE_NAME, Context.MODE_PRIVATE);
-        mPrefs.edit().putString(CURRENT_USER, JsonUtils.encode(user)).commit();
+        getPrefs(context).edit().putString(CURRENT_USER, JsonUtils.encode(user)).commit();
     }
 
     public static void removeCurrentUser(Context context) {
-        SharedPreferences mPrefs = context.getSharedPreferences(Constants.PACKAGE_NAME, Context.MODE_PRIVATE);
-        mPrefs.edit().remove(CURRENT_USER).commit();
+        getPrefs(context).edit().remove(CURRENT_USER).commit();
     }
 }
